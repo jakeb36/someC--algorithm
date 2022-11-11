1f503fd883a64b2a39bfccf8196a80bea98fed10
Rabnum <66543806+jakeb36@users.noreply.github.com>
2022-11-11 10:46:01 +0800

    Update Ackmann函数.cpp

--- Ackmann函数.cpp
+++ Ackmann函数.cpp
@@ -1,3 +1,14 @@
+/*
+阿克曼(Ackmann)函数A(m，n)中，m，n定义域是非负整数(m≤3,n≤10)，函数值定义为：
+
+akm(m,n)= 
+ 
+n+1(m=0时)
+akm(m−1,1)(m>0,n=0时)
+akm(m−1,akm(m,n−1))(m,n>0时)
+
+*/
+
 #include <iostream>
 using namespace std;
 int akm(int m, int n)
